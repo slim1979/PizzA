ce7cf2b4452ea3abfde7eb0ad242c3ae3a73e2fa
slim1979 <chinezan@yandex.ru>
2017-03-05 19:53:47 +0300

    изменена переменная localStorage

--- public/pizza.js
+++ public/pizza.js
@@ -1,9 +1,26 @@
-
+//
+// function something ()
+// {
+//
+// 	var x= window.localStorage.getItem('aaa') // сейчас х имеет тип string
+//
+// 	x = x*1 + 1 //умножаем х на 1 для того, чтобы сменить тип переменной со string на int
+//
+// 	window.localStorage.setItem('aaa',x)
+//
+// 	alert(x);
+//
+// }
 
 function something ()
 {
-	var x= window.localStorage.getItem('aaa')
-	window.localStorage.setItem('aaa',555)
+
+	var x= window.localStorage.getItem('bbb') // сменили aaa на bbb чтобы обнулить значение х
+
+	x = x*1 + 1 //умножаем х на 1 для того, чтобы сменить тип переменной со string на int
+
+	window.localStorage.setItem('bbb',x)
+
 	alert(x);
 
 }
